5260a1acf33bc1a4de85887b33aab2a97a7b2657
Brindulos <bmenager@gmail.com>
2025-04-08 09:26:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salut, c’est Jessy 💋")
 
-app = ApplicationBuilder().token("123456789:ABCdefGhIJKlMNOpQRstuVWxyZ").build()
+app = ApplicationBuilder().token("7909147695:AAGxDZVEOiuHRtwTCgpV-1FMpNE16iT9W40").build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
