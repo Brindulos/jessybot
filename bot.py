907506a3eeffeeca5299ebcf11bcf537bf78d595
Brindulos <bmenager@gmail.com>
2025-04-08 11:44:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salut, c’est Jessy 💋")
-
 import os
-app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
-app.add_handler(CommandHandler("start", start))
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-app.run_polling()
+async def start(update, context):
+    await update.message.reply_text("Hello toi... Moi c’est Jessy. 😏")
+
+async def echo(update, context):
+    await update.message.reply_text("Tu viens de dire : " + update.message.text)
+
+if __name__ == '__main__':
+    token = os.getenv("TELEGRAM_TOKEN")
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
